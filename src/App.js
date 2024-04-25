85166e404c7fd47198c6670b0dea78cc26de7086
SamiPetajajarvi <150699937+SamiPetajajarvi@users.noreply.github.com>
2024-04-25 17:10:35 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import './App.css';
-import Navbar from './components/navbar';
-import Notifications from './components/notifications';
-import Header from './components/header';
-import Content from './components/content';
-import Footer from './components/footer';
+import React, { useState } from 'react';  
+import axios from 'axios';
+import SignIn from './SignIn'; 
+import DeleteAcccount from './DeleteAccount';
 
-function App() {
+
+
+
+
+function SignUp() {
+  const [email, setEmail] = useState('');
+  const [pw, setPassword] = useState('');
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [username, setUsername] = useState('');
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3001/SignUp', { email, pw, fname, lname, username });
+      console.log('Sign up successful:', response.data);
+      
+      
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+      
+
+    };
+    
+
+
   return (
-    <div className="App">
-      <Navbar />
-      <Notifications />
-      <Header />
-      <Content />
-      <Footer/>
-      </div>
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSignUp}>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+        <input type="password" value={pw} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        <input type="fname" value={fname} onChange={(e) => setFname(e.target.value)} placeholder="First name" />
+        <input type="Lname" value={lname} onChange={(e) => setLname(e.target.value)} placeholder="Last name" />
+        <input type="Username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+        <button type="submit">Sign Up</button>
+      </form>
+      
+      <SignIn />
+      <DeleteAcccount />
+    </div>
   );
-
 }
 
-export default App;
+export default SignUp;
+
