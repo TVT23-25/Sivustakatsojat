b997a2c2563b85aaf8792446f2c1246271823e63
Virvelin <148015070+Virvelin@users.noreply.github.com>
2024-05-02 19:05:15 +0300

    Update NewTest.js

--- NewTest.js
+++ NewTest.js
@@ -1,9 +1,23 @@
-// Lataa tarvittavat 
+// tarvittavat
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
 const App = require('../App');
+
 chai.use(chaiHttp);
+
+
+// Määritä testit käyttäjätilin hallinnalle
+describe('Käyttäjätilin hallinta', () => {
+    // Määritä käyttäjätiedot
+    const userCredentials = {
+        username: 'testuser',
+        pw: 'testpassword',
+        email: 'testuser@example.com',
+        fname: '',
+        lname: '',
+    };
+})
 
 // Testi uuden käyttäjän rekisteröintiä
 it('Rekisteröi uuden käyttäjän', () => {
@@ -20,4 +34,57 @@ it('Rekisteröi uuden käyttäjän', () => {
             done();
         });
 })
+
+  // Testaa sisäänkirjautumista rekisteröidyllä käyttäjällä
+  it('Kirjaudu sisään rekisteröidyllä käyttäjällä', () => {
+    // Tee HTTP POST -pyyntö
+    chai.request(App)
+        .post('/SignIn')
+        .send({
+            email: userCredentials.email,
+                pw: userCredentials.pw,
+        })
+        .end((err, res) => {
+            // Tarkista 
+            chai.expect(res).to.have.status(401);
+            chai.expect(res.body).to.have.property('success').to.be.true;
+            chai.expect(res.body).to.have.property('token');
+            chai.expect(res.body).to.have.property('message').to.equal('Autentikaatio onnistui');
+            done();
+        });
+});
+
+    // Testaa sisäänkirjautumista
+    it('Kirjaudu sisään', (done) => {
+    // Tee HTTP POST -pyyntö
+    chai.request(App)
+        .post('/SignIn')
+        .send({
+            email: userCredentials.email,
+            pw: userCredentials.pw
+        })
+        .end((err, res) => {
+            // Tarkista vastaus
+            chai.expect(res).to.have.status(200);
+            chai.expect(res.body).to.have.property('success').to.be.true;
+            chai.expect(res.body).to.have.property('message').to.equal('Kirjautuminen onnistui');
+            chai.expect(res.body).to.have.property('token');
+            done();
+        });
+});
+
+    // testaa poistoa
+    it('Poista rekisteröity käyttäjä', (done) => {
+        // Tee HTTP DELETE -pyyntö
+        chai.request(App)
+        .delete(`/deleteAccount/${userCredentials.username}`)
+        .end((err, res) => {
+            // Tarkista
+            expect(res).to.have.status(200);
+            expect(res.body).to.have.property('success').to.be.true;
+            expect(res.body).to.have.property('message').to.equal('Käyttäjätunnuksen poistaminen onnistui');
+            done();
+        });
+});
+    
 
